047b0155005ea52f0e90228b830cfca596b21442
aminelli <antonio.minelli@etlforma.com>
2025-10-16 11:53:07 +0200

    upd

---
+++ Environment/gitlab/99-destroy.bat
@@ -0,0 +1,4 @@
+@echo off
+
+docker compose -p gitlab-studio down -v
+
---
+++ Environment/portainer/install-portainer.bat
@@ -0,0 +1,4 @@
+@echo off
+
+docker volume create portainer_data
+docker run -d -p 8900:8000 -p 9943:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
